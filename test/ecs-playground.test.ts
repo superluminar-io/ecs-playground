e7b2de325f1690857a3e778963852bce4460f11d
Henrik Fricke <henrikfricke@web.de>
2022-09-19 15:33:04 +0200

    Initial commit

---
+++ test/ecs-playground.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsPlayground from '../lib/ecs-playground-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-playground-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsPlayground.EcsPlaygroundStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
